2f6461b471bc6ee46a09a5cccdff7b96a237e62d
yangho1231 <yangho1231@gmail.com>
2016-11-16 14:20:26 -0700

    Did the steps

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors');
+var massive = require('massive');
+var config = require('./config.js');
+
+
+var massiveInstance = massive.connectSync({connectionString : config.connectionString});
+
+
+
 
 var app = module.exports = express();
 app.use(bodyParser.json());
 app.use(cors());
+app.set('db', massiveInstance);
+var db = app.get('db');
+var controller = require('./controller.js');
+controller.getPlanes();
+
+// db.new_plane(function(err, planes) {
+//   console.log(err, "plane added");
+// });
+
+// db.get_planes(function(err, planes){
+//     console.log(err, planes)
+// })
+
 
 app.listen('3000', function(){
-  console.log("Successfully listening on : 3000")	
-})
-
+  console.log("Successfully listening on : 3000");
+});
